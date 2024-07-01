9f35f9d9c70a3b07e81b698a85dc2aaa0d8e96b2
Nesh <soninesh2006@gmail.com>
2024-06-30 19:03:42 -0600

    Modified

--- public/script.js
+++ public/script.js
@@ -1,5 +1,36 @@
+// async function searchWord() {
+//   const term = document.getElementById('search-term').value;
+//   const response = await fetch(`/api/word/${term}`);
+//   const result = await response.json();
+
+//   if (response.status === 404) {
+//     document.getElementById('result').innerHTML = '<p>Word not found</p>';
+//   } else {
+//     document.getElementById('result').innerHTML = `
+//       <h2 style="color: white; font-size: 24px;">${term}</h2>
+//       <p style="color:white; font-size:15px; font-weight:normal; font-family:sans-serif;"> 
+//         <strong style="color:#bb86fc;">Part of Speech:</strong> ${result.partOfSpeech}
+//       </p>
+//       <p style="color:white; font-size:15px; font-weight:normal; font-family:sans-serif;"> 
+//         <strong style="color:#bb86fc; max-width:20%">Definition:</strong> ${result.definition}
+//       </p>
+//       <p style="color:white; font-size:15px; font-weight:normal; font-family:sans-serif;"> 
+//         <strong style="color:#bb86fc;">Example:</strong> ${result.example}
+//       </p>
+//       <p style="color:white; font-size:15px; font-weight:normal; font-family:sans-serif;"> 
+//         <strong style="color:#bb86fc;">Pronunciation:</strong> ${result.pronunciation}
+//       </p>
+//         <strong style="color:#bb86fc;">Synonyms:</strong> ${result.synonyms.join(', ')}
+//       </p>
+//       <p style="color:white; font-size:15px; font-weight:normal; font-family:sans-serif;"> 
+//         <strong style="color:#bb86fc;">Antonyms:</strong> ${result.antonyms.join(', ')}
+//       </p>
+//     `;
+//   }
+// }
+
 document.getElementById('search-button').addEventListener('click', searchWord);
-document.getElementById('search-button').addEventListener('touchstart', searchWord);
+// document.getElementById('search-button').addEventListener('touchstart', searchWord);
 
 async function searchWord(event) {
   event.preventDefault(); // Prevent default action for touch events
